d8add4d9c6f86b2632ec12edc1142edcfdb89d65
Thomas Lau <thomasklau@gmail.com>
2015-02-21 03:54:40 -0800

    asdfasdf

--- DiffbotParsing/test.rb
+++ DiffbotParsing/test.rb
@@ -1,6 +1,6 @@
 require "net/http"
 require "json"
 require "rubygems"
-response = Net::HTTP.get_response("diffbot.com","/api/CS103?token=73659789c3c2aa6d84fd9812b5ba57dc&url=http://www.stanford.edu/class/archive/cs/cs143/cs143.1128/")
+response = Net::HTTP.get_response("diffbot.com","/api/CS103?token=73659789c3c2aa6d84fd9812b5ba57dc&url=http://web.stanford.edu/class/archive/cs/cs166/cs166.1146/")
 my_JSON = JSON.parse(response.body) #this must show the JSON contents
-p my_JSON
+p my_JSON["AnnouncmentContent"][0]["description"]
