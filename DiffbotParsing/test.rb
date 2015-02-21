b3673614b4fb02a0f935823a0e88fa2353ccfa79
Sidhanth Grover <sidhanth@stanford.edu>
2015-02-21 04:22:46 -0800

    ruby

--- DiffbotParsing/test.rb
+++ DiffbotParsing/test.rb
@@ -2,5 +2,16 @@ require "net/http"
 require "json"
 require "rubygems"
 response = Net::HTTP.get_response("diffbot.com","/api/CS103?token=73659789c3c2aa6d84fd9812b5ba57dc&url=http://web.stanford.edu/class/archive/cs/cs166/cs166.1146/")
-my_JSON = JSON.parse(response.body) #this must show the JSON contents
-p my_JSON["AnnouncementBody"][0]["Description"]
+json = JSON.parse(response.body) #this must show the JSON contents
+# p json["AnnouncementBody"][0]["Description"]
+# p json
+
+i = 0
+json["AnnouncementBody"]. each do |anna|
+	p json["AnnouncementHeader"][i]["Header"]
+	p json["AnnouncementDate"][i]["Date"]
+	p json["AnnouncementBody"][i]["Description"]
+	p "------------"
+
+	i = i + 1 
+end
