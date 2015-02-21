408b547aa42ad222223f1b90672d46e1e60a22b4
Sidhanth Grover <sidhanth@stanford.edu>
2015-02-21 09:40:35 -0800

    shit

--- DiffbotParsing/addNewCourse.rb
+++ DiffbotParsing/addNewCourse.rb
@@ -6,16 +6,30 @@ courseName = ARGV[0]
 courseURL = ARGV[1]
 
 course = Course.new(name: CourseName)
+diffbotURLToken = ""
+api = ""
 
+case courseName
+	when courseName = "CS 106B"
+        api = "CS106B"
+	when courseName = "CS 103"
+	    api = "CS103"
+	when courseName = "CS 106L"
+	    api = "CS106L"
+	when courseName = "CS 166"
 
+	when courseName = "CS 161"
+	
+	when courseName = "CS 143"
+
+	when courseName = "CS 106A"	
+end	
 
 
 
 response = Net::HTTP.get_response("diffbot.com","/api/CS103?token=73659789c3c2aa6d84fd9812b5ba57dc&url=http://web.stanford.edu/class/archive/cs/cs166/cs166.1146/")
-<<<<<<< HEAD
 my_JSON = JSON.parse(response.body) #this must show the JSON contents
 p my_JSON["AnnouncementBody"]
-=======
 json = JSON.parse(response.body) #this must show the JSON contents
 # p json["AnnouncementBody"][0]["Description"]
 # p json
@@ -29,4 +43,4 @@ json["AnnouncementBody"]. each do |sivayetski|
 
 	i = i + 1 
 end
->>>>>>> FETCH_HEAD
+
